9d93058ee3a32e50e457ee3b05b726fde37187bf
SarahKamunya <sarahkamunya254@gmail.com>
2022-03-13 23:40:24 +0300

    add jquery to design icon

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $('#design_img').on('click', function(){
+        $('#design_img').hide();
+        $('.design_head').toggle();
+        $('.design_text').toggle();
+
+    })
+
+    $('.design_text').on('click', function(){
+        $('.design_head').hide();
+        $('#design_img').show();
+        $('.design_text').hide();
+
+    })
+
+    // $('.design_text').on('click', function(){
+    //     $('.design_head').toggle();
+    //     $('#design_img').toggle();
+    //     $('.design_text').toggle();
+
+    // })
+
+    // $('#dev_img').on('click', function(){
+    //     $('.dev_head').toggle();
+    //     $('#dev_img').toggle();
+    //     $('.dev_text').toggle();
+
+    // })
+
+    // $('.dev_head').on('click', function(){
+    //     $('.dev_head').toggle();
+    //     $('#dev_img').toggle();
+    //     $('.dev_text').toggle();
+
+    // })
+
+    // $('.dev_text').on('click', function(){
+    //     $('.dev_head').toggle();
+    //     $('#dev_img').toggle();
+    //     $('.dev_text').toggle();
+
+    // })
+    
+
+  
+
+
+});
